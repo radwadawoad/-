cb3e7b56316d4ff716b948a0a0ee721feb7bd552
radwadawoad <50420973+radwadawoad@users.noreply.github.com>
2022-10-23 16:56:31 +0200

    Add files via upload

---
+++ terraform/pub_sub.tf
@@ -0,0 +1,15 @@
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+map_public_ip_on_launch= "true"
+  
+  }
+
+
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+map_public_ip_on_launch= "true"
+  
+  }
+
